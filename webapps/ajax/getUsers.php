4852c3f3fad58bf80228b2fff322d22c2a783cbe
Agnieszka Sobociñska <s174966@v-ie.uek.krakow.pl>
2017-05-13 18:04:11 +0200

    typo

--- webapps/ajax/getUsers.php
+++ webapps/ajax/getUsers.php
@@ -4,7 +4,7 @@ header("Content-Type: application/json; charset=UTF-8");
 
 $conn = new mysqli("10.254.94.2", "s174966", "PgsHqxfj", "s174966");
 
-$result = $conn->query("SELECT id, firstName, lastName FROM Customers");
+$result = $conn->query("SELECT id, firstName, lastName FROM user");
 
 $outp = "";
 while($rs = $result->fetch_array(MYSQLI_ASSOC)) {

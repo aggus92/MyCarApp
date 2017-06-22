90026f61ae1d62ffd821cdc3ea987c8a0f836baf
Agnieszka Sobociñska <s174966@v-ie.uek.krakow.pl>
2017-06-22 19:22:58 +0200

    change display date

--- js/controllers/RemindersCtrl.js
+++ js/controllers/RemindersCtrl.js
@@ -18,11 +18,11 @@ mycarapp.controller('RemindersCtrl', ['$scope', '$rootScope', '$stateParams', '$
             }).then(function (response) {
                 console.log(response);
                 if (response.data.records != undefined && response.data.records.length > 0) {
-                    var endDate = new Date();
-                    endDate.setDate(endDate.getDate() + 365);
                     var currentDate = new Date();
                     var date = new Date(response.data.records[0].date);
                     var daysToEnd = 365 - (Math.round((currentDate.getTime() - date.getTime()) / (1000 * 60 * 60 * 24)));
+					var endDate = new Date();
+                    endDate.setDate(date.getDate() + 365);
                     $scope.remindersList.push({
                         type: 'COMMON_COST_TECH_REVIEW',
                         date: response.data.records[0].date,
@@ -44,11 +44,11 @@ mycarapp.controller('RemindersCtrl', ['$scope', '$rootScope', '$stateParams', '$
             }).then(function (response) {
                 console.log(response);
                 if (response.data.records != undefined && response.data.records.length > 0) {
-                    var endDate = new Date();
-                    endDate.setDate(endDate.getDate() + 365);
                     var currentDate = new Date();
                     var date = new Date(response.data.records[0].date);
                     var daysToEnd = 365 - (Math.round((currentDate.getTime() - date.getTime()) / (1000 * 60 * 60 * 24)));
+					var endDate = new Date();
+                    endDate.setDate(date.getDate() + 365);
                     $scope.remindersList.push({
                         type: 'COMMON_COST_INSURANCE',
                         date: response.data.records[0].date,
